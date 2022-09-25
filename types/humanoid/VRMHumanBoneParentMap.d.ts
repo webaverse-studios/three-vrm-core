9df2386b3899eb70acf19de9cd10cf96decf17bd
shawticus <shawmakesmagic@gmail.com>
2022-09-25 13:29:46 -0700

    1.0.2

--- types/humanoid/VRMHumanBoneParentMap.d.ts
+++ types/humanoid/VRMHumanBoneParentMap.d.ts
@@ -2,7 +2,7 @@ import { VRMHumanBoneName } from './VRMHumanBoneName';
 /**
  * An object that maps from {@link VRMHumanBoneName} to its parent {@link VRMHumanBoneName}.
  *
- * Ref: https://github.com/vrm-c/vrm-specification/blob/master/specification/VRMC_vrm-1.0-beta/humanoid.md
+ * Ref: https://github.com/vrm-c/vrm-specification/blob/master/specification/VRMC_vrm-1.0/humanoid.md
  */
 export declare const VRMHumanBoneParentMap: {
     [bone in VRMHumanBoneName]: VRMHumanBoneName | null;
